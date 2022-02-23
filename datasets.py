a432d86408a023d0aac11a1e0f311e4ffd768e15
Luisa_Local <luisa.neubauer999@gmail.com>
2022-02-23 09:16:01 +0100

    get and len method for DIV2K

--- datasets.py
+++ datasets.py
@@ -2,11 +2,13 @@ import h5py
 import numpy as np
 import os
 import io
-
 import torch
 from torch.utils.data import Dataset
+from PIL import Image
+import matplotlib.pyplot as plt
+from skimage import io
 
-
+"""Datasets for H5 """
 class TrainDataset(Dataset):
     def __init__(self, h5_file):
         super(TrainDataset, self).__init__()
@@ -19,7 +21,6 @@ class TrainDataset(Dataset):
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
             return len(f['lr'])
-
 
 class EvalDataset(Dataset):
     def __init__(self, h5_file):
@@ -68,19 +69,57 @@ class BSDS500(Dataset):
         return sample
 
 
-
 class DIV2K(Dataset):
     """Dataset from the Berkeley Segmentation Data Set and Benchmarks 500 (BSDS500)"""
     def __init__(self, root_dir, transform=None):
         """
         Args:
             root_dir (string): Directory with all the images.
+            here: "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/3D_vision_super_resolution/data/DIV2K_valid_HR"
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        
+        self.root_dir = root_dir
+        self.transform = transform
+
     def __len__(self):
+        list = os.listdir(self.root_dir)  # dir is your directory path
+        onlyfiles = sorted(next(os.walk(self.root_dir))[2])  # dir is your directory path as string
+        print(onlyfiles)
+        #len(onlyfiles)
+        number_files = len(list)
+        return number_files
+
+        #L = len([name for name in os.listdir('.') if os.path.isfile(name)])
+
+
+    def __getitem__(self, idx, show_image = False):
+        onlyfiles = sorted(next(os.walk(self.root_dir))[2])
+        img_name = onlyfiles[idx]
+        img_path = self.root_dir + '/' + img_name
+
+        #Reading the File as PIL Image
+        img = Image.open(img_path)
+        print(type(img))
+
+        #Reading using scikit-image
+        img = io.imread(img_path)
+        print(type(img))
+
+        if show_image:
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(img)
+            plt.show()
+
+        if self.transform is not None:
+            #do transform
+            pass
+
         pass
 
-    def __getitem__(self, idx):
-        pass
+
+if __name__ == '__main__':
+    dataset = DIV2K(root_dir="/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/3D_vision_super_resolution/data/DIV2K_valid_HR")
+    len(dataset)
+    dataset.__getitem__(idx=1, show_image=True)
