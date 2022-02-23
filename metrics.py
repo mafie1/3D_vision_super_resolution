243f2a36020309be630c906a10e0d7c6760fd015
Luisa_Local <luisa.neubauer999@gmail.com>
2022-02-23 09:51:45 +0100

    PSNR Implementation + Template SSIM

---
+++ metrics.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+
+
+def PSNR(pred, gt, shave_border=0):
+    height, width = pred.shape[:2]
+    pred = pred[shave_border:height - shave_border, shave_border:width - shave_border]
+    gt = gt[shave_border:height - shave_border, shave_border:width - shave_border]
+    imdff = pred - gt
+    rmse = math.sqrt(np.mean(imdff ** 2))
+    if rmse == 0:
+        return 100
+    return 20 * math.log10(255.0 / rmse) #db (logarithmic scale)
+
+def SSIM(pred, gt):
+    """Implementation of the Structural Similarity Index """
+    pass
+
