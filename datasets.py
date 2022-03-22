7e4aa1605eee8d59d5cceef2400f8c020cbf0772
Luisa_Local <luisa.neubauer999@gmail.com>
2022-03-22 09:07:16 +0100

    Split Datasets into relevant and irrelevant parts

--- datasets.py
+++ datasets.py
@@ -25,6 +25,7 @@ class TrainDatasetH5(Dataset):
         with h5py.File(self.h5_file, 'r') as f:
             return len(f['lr'])
 
+
 class EvalDatasetH5(Dataset):
     def __init__(self, h5_file):
         super(EvalDatasetH5, self).__init__()
@@ -41,30 +42,55 @@ class EvalDatasetH5(Dataset):
 
 class BSDS500(Dataset):
     """Dataset from the Berkeley Segmentation Data Set and Benchmarks 500 (BSDS500)"""
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir=None, transform=None, mode='train'):
         """
         Args:
             root_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
+            train:
         """
-        self.root_dir = root_dir
+        if root_dir:
+            self.root = root_dir + mode
+        else:
+            self.root = '/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/BSR/BSDS500/images/'+mode
+
+        print(self.root)
         self.transform = transform
+        self.mode = mode
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        onlyfiles = sorted(next(os.walk(self.root))[2])  # dir is your directory path as string
+        number_files = len(onlyfiles)
+        return number_files
 
     def __getitem__(self, idx):
+
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0])
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks])
-        landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'landmarks': landmarks}
+        onlyfiles_HR = sorted(next(os.walk(self.root))[2])
+        img_path_HR = self.root + '/' + onlyfiles_HR[idx]
+        img_HR = io.imread(img_path_HR)
+
+        img_LR = img_HR #to do: downsample bicubic
+
+
+        if self.transform is not None:
+            img_HR = self.transform(img_HR)
+            img_LR = self.transform(img_LR)
+
+        print('SIZE HR image:', img_HR.shape)
+        print('SIZE LR image:', img_LR.shape)
+        return img_LR, img_HR
+
+       # img_name = os.path.join(self.root_dir,
+       #                        self.landmarks_frame.iloc[idx, 0])
+        #image = io.imread(img_name)
+        #landmarks = self.landmarks_frame.iloc[idx, 1:]
+        #landmarks = np.array([landmarks])
+        #landmarks = landmarks.astype('float').reshape(-1, 2)
+        #sample = {'image': image, 'landmarks': landmarks}
 
         if self.transform is not None:
             sample = self.transform(sample)
@@ -73,131 +99,22 @@ class BSDS500(Dataset):
         return sample
 
 
-class DIV2K(Dataset):
-    """Dataset from the Berkeley Segmentation Data Set and Benchmarks 500 (BSDS500) with bicubic 2x downsampling"""
-    def __init__(self, root_HR, root_LR, transform=None):
-        """
-        Args:
-            root_HR (string): Directory path with the original HR images. Those are the labels
-                            here: "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/3D_vision_super_resolution/data/DIV2K_valid_HR"
-            root_LR (string): Directory path with the low-resolution (downnsampled) images.
-                            Different downsampling methods are available (bicubic, ...)
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.root_dir_HR = root_HR
-        self.root_dir_LR = root_LR
-        self.transform = transform
-
-    def __len__(self):
-        #list = os.listdir(self.root_dir)  # dir is your directory path
-        onlyfiles = sorted(next(os.walk(self.root_dir_HR))[2])  # dir is your directory path as string
-        number_files = len(onlyfiles)
-        return number_files
-
-    def __getitem__(self, idx):
-        onlyfiles_HR = sorted(next(os.walk(self.root_dir_HR))[2])
-
-        img_path_HR = self.root_dir_HR + '/' + onlyfiles_HR[idx]
-        img_path_LR = self.root_dir_LR + '/' + onlyfiles_HR[idx][:-4] + 'x2.png'
-
-        #Reading the File as PIL Image
-        #img_HR = Image.open(img_path_HR)
-        #img_LR = Image.open(img_path_LR)
-        #print(type(img_HR))
-
-        #Reading using scikit-image
-        img_HR = io.imread(img_path_HR) #full-sized image in HR
-        img_LR = io.imread(img_path_LR)
-        #print(type(img_HR))
-
-        #TO DO: standardize size of all LR and HR images or crop
-        #H, W, C = img_HR.shape
-        #img_LR = resize(img_LR, output_shape= (H,W,C), order = 0)
-
-        if self.transform is not None:
-            img_HR = self.transform(img_HR)
-            img_LR = self.transform(img_LR)
-
-        print('SIZE HR image:', img_HR.shape)
-        print('SIZE LR image:', img_LR.shape)
-        return img_LR, img_HR
-
-
-class domain_transfer_DS(Dataset): #unfinished
-    """Dataset from the Berkeley Segmentation Data Set and Benchmarks 500 (BSDS500) with bicubic 2x downsampling"""
-    def __init__(self, root_HR, root_LR, transform=None, domain1=True):
-        """
-        Args:
-            root_HR (string): Directory path with the original HR images. Those are the labels.
-            root_LR (string: Directory path with low resolution images
-
-            domain1 (bool): True for domain 1 (e.g. Dogs), False for domain 2 (e.g.Cars).
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.root_dir_HR = root_HR
-        self.root_dir_LR = root_LR
-        self.domain_one = domain1
-
-    def __len__(self):
-        #list = os.listdir(self.root_dir)  # dir is your directory path
-        onlyfiles = sorted(next(os.walk(self.root_dir_HR))[2])  # dir is your directory path as string
-        number_files = len(onlyfiles)
-        return number_files
-
-    def __getitem__(self, idx):
-        #if self.domain_one:
-         #   pass
-
-        onlyfiles_HR = sorted(next(os.walk(self.root_dir_HR))[2])
-
-        img_path_HR = self.root_dir_HR + '/' + onlyfiles_HR[idx]
-        img_path_LR = self.root_dir_LR + '/' + onlyfiles_HR[idx][:-4] + 'x2.png'
-
-        #Reading the File as PIL Image
-        #img_HR = Image.open(img_path_HR)
-        #img_LR = Image.open(img_path_LR)
-        #print(type(img_HR))
-
-        #Reading using scikit-image
-        img_HR = io.imread(img_path_HR) #full-sized image in HR
-        img_LR = io.imread(img_path_LR)
-        #print(type(img_HR))
-
-        #TO DO: standardize size of all LR and HR images or crop
-        #H, W, C = img_HR.shape
-        #img_LR = resize(img_LR, output_shape= (H,W,C), order = 0)
-
-        if self.transform is not None:
-            img_HR = self.transform(img_HR)
-            img_LR = self.transform(img_LR)
-
-        print('SIZE HR image:', img_HR.shape)
-        print('SIZE LR image:', img_LR.shape)
-        return img_LR, img_HR
-
 
 if __name__ == '__main__':
-    ROOT_labels= "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/DIV2K_train_HR"
-    ROOT_images = "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/DIV2K_train_LR_bicubic/X2"
-
-    dataset_valid = DIV2K(root_HR=ROOT_labels, root_LR = ROOT_images)
-    print(len(dataset_valid))
-
-    LR_image, HR_image = dataset_valid.__getitem__(idx=2)
-
-    plt.imshow(LR_image)
-    plt.show()
-
-    plt.imshow(HR_image)
-    plt.show()
+    #ROOT_labels= "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/DIV2K_train_HR"
+    #ROOT_images = "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/DIV2K_train_LR_bicubic/X2"
+    #dataset_valid = DIV2K(root_HR=ROOT_labels, root_LR = ROOT_images)
+    #LR_image, HR_image = dataset_valid.__getitem__(idx=2)
+    #plt.imshow(LR_image)
+    #plt.show()
+    #plt.imshow(HR_image)
+    #plt.show()
 
 
-    #TRAIN_FILE = "data/91-image_x2.h5"
-    #dataset_h5 = TrainDatasetH5(TRAIN_FILE)
+    TRAIN_FILE = "/Users/luisaneubauer/Documents/WS 2021:22/3D Reconstruction/super_resolution/data/Set5/91-image_x4.h5"
+    dataset_h5 = TrainDatasetH5(TRAIN_FILE)
+    image, label = dataset_h5.__getitem__(1)
 
-    #image, label = dataset_h5.__getitem__(1)
-
-    """
     plt.imshow(image[0,:,:], cmap='gray', interpolation='bicubic')
     plt.show()
     plt.imshow(label[0,:,:], cmap='gray', interpolation='bicubic')
@@ -205,7 +122,11 @@ if __name__ == '__main__':
 
     print(image.shape)
     print(label.shape)
-    print(type(image))"""
+
+    #dataset = BSDS500(mode='test')
+    #print(len(dataset))
+
+    #image, label = dataset.__getitem__(1)
 
 
 
