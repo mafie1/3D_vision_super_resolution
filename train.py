2ba7c6fcc948b807efbda9baf03ab6db19888cba
Luisa_Local <luisa.neubauer999@gmail.com>
2022-03-23 14:51:13 +0100

    Working Version

--- train.py
+++ train.py
@@ -152,8 +152,8 @@ def train_function(model, criterion, optimizer,
 if __name__ == '__main__':
     SEED = 0
     LEARNING_RATE = 5e-4
-    NUM_EPOCHS = 20
-    BATCH_SIZE = 8
+    NUM_EPOCHS = 2
+    BATCH_SIZE = 4
     NUM_WORKERS = 0
     DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     CHANNELS = 3
@@ -169,9 +169,9 @@ if __name__ == '__main__':
     TRAIN_DATASET, EVAL_DATASET = train_test_split(dataset_small, test_size=0.1, random_state=SEED)
     print('Train-Eval-Split is done. \nThere are x images in the \nTraining Set: {}\nEval Set: {}'.format(len(TRAIN_DATASET), len(EVAL_DATASET)))
 
-    OUT_DIR = "outputs"
-   #MODEL = SRCNN(num_channels=CHANNELS).to(DEVICE).double()  #num_channels = 1 for gray scale images, 3 for color images
-    MODEL = VDSR(num_channels=CHANNELS, d=4).to(DEVICE).float()  #default is three channel (e.g. RGB) images
+    OUT_DIR = "outputs/SRCNN-BSD100-X4"
+    MODEL = SRCNN(num_channels=CHANNELS).to(DEVICE).double()  #num_channels = 1 for gray scale images, 3 for color images
+    #MODEL = VDSR(num_channels=CHANNELS, d=4).to(DEVICE).float()  #default is three channel (e.g. RGB) images
 
     OPTIMIZER = optim.Adam(MODEL.parameters(), lr=LEARNING_RATE)  # all training, later: train head and backbone separate
     #OPTIMIZER = optim.SGD(MODEL.parameters(), lr=LEARNING_RATE)
